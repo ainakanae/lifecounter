893901717eb15bbad17703a016a7ce341dde4211
Aina Engelbrekt <ainae@uw.edu>
2024-04-22 13:31:47 -0700

    player 1 working

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -14,6 +14,45 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    var p1Lives = 20
+    var p2Lives = 20
+    
+    
+    @IBOutlet var banner: UILabel!
+    
+    @IBOutlet var player1reference: UILabel!
+    
+    
+    @IBAction func p1minus1(_ sender: Any) {
+        p1Lives -= 1
+        changePlayer1Lives()
+    }
+    
+    @IBAction func p1plus1(_ sender: Any) {
+        p1Lives += 1
+        changePlayer1Lives()
+    }
+    
+    @IBAction func p1minus5(_ sender: Any) {
+        p1Lives -= 5
+        changePlayer1Lives()
+    }
+    
+    @IBAction func p1plus5(_ sender: Any) {
+        p1Lives += 5
+        changePlayer1Lives()
+    }
+    
+    func changePlayer1Lives(){
+        player1reference.text = "Player 1 lives:  \(p1Lives)"
+        p1LoserBanner()
 
+    }
+    
+    func p1LoserBanner() {
+        if p1Lives <= 0 {
+            banner.text = "Player 1 Loses!"
+        }
+    }
 }
 
